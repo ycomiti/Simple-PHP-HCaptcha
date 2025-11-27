10d13ff833d3b1d001f51b2471a3ca8f105d0c1d
ycomiti <94963509+ycomiti@users.noreply.github.com>
2025-11-27 16:56:49 +0100

    Update IPUtils.php

--- src/hcaptcha/utils/IPUtils.php
+++ src/hcaptcha/utils/IPUtils.php
@@ -4,9 +4,9 @@ namespace hcaptcha\utils;
 class IPUtils {
 
   public function getRemoteIP(): ?string {
-	if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) return $this->getValid($_SERVER['HTTP_CF_CONNECTING_IP']);
-	if (isset($_SERVER['HTTP_CLIENT_IP'])) return $this->getValid($_SERVER['HTTP_CLIENT_IP']);
-	return $this->getValid($_SERVER['REMOTE_ADDR']);
+  	if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) return $this->getValid($_SERVER['HTTP_CF_CONNECTING_IP']);
+  	if (isset($_SERVER['HTTP_CLIENT_IP'])) return $this->getValid($_SERVER['HTTP_CLIENT_IP']);
+  	return $this->getValid($_SERVER['REMOTE_ADDR']);
   }
 
   protected function getValid(?string $ip): ?string {
@@ -16,5 +16,5 @@ class IPUtils {
       filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)
     ) ? $ip : null;
   }
-  
+
 }
